74ecae2a0b8deb4188b54c1335f2172554d3527f
xxai.art <xxai.art@gmail.com>
2023-07-11 10:44:25 +0800

    ♨

---
+++ src/nav/Lang.svelte
@@ -0,0 +1,58 @@
+<script lang="coffee">
+> svelte > onMount
+  wac.tax/wtax/On.js
+
+FULL = !! document.fullscreenElement
+
+onMount =>
+  On(
+    document
+    fullscreenchange: =>
+      FULL = !! document.fullscreenElement
+      return
+  )
+
+full = =>
+  if document.fullscreenElement
+    await document.exitFullscreen()
+  else
+    await document.body.requestFullscreen()
+  return
+
+</script>
+
+<template lang="pug">
+u-i18n
+b
+  a.ft(@click=full class:f=FULL)
+  a.gg(href="//groups.google.com/g/xxai-art" target="_blank")
+</template>
+
+<style lang="stylus">
+b
+  display flex
+  margin-bottom -3px
+  position relative
+
+  &>a
+    height 42px
+    margin-left 25px
+    margin-right 0
+    width 25px
+
+    &.ft
+      background url(':/svg/full.svg') 50% 50% / 20px no-repeat
+
+      &.f
+        background-image url(':/svg/unfull.svg')
+
+    &.gg
+      background url(':/svg/google_groups.svg') 50% 50% / 50px no-repeat
+
+u-i18n
+  margin-bottom -4px
+  opacity 0.75
+  transform scale(0.76)
+  transform-origin right 50%
+</style>
+
--- src/nav/Nav.svelte
+++ src/nav/Nav.svelte
@@ -1,23 +1,5 @@
 <script lang="coffee">
-> svelte > onMount
-  wac.tax/wtax/On.js
 
-FULL = !! document.fullscreenElement
-
-onMount =>
-  On(
-    document
-    fullscreenchange: =>
-      FULL = !! document.fullscreenElement
-      return
-  )
-
-full = =>
-  if document.fullscreenElement
-    await document.exitFullscreen()
-  else
-    await document.body.requestFullscreen()
-  return
 
 
 </script>
@@ -27,11 +9,7 @@ nav
   i
     slot
   i
-    slot(name="rl")
-    u-i18n
-    b
-      a.ft(@click=full class:f=FULL)
-      a.gg(href="//groups.google.com/g/xxai-art" target="_blank")
+    slot(name="R")
 </template>
 
 <style lang="stylus">
@@ -49,33 +27,5 @@ nav
     display flex
     font-style initial
     margin 0 40px
-
-    &>b
-      position relative
-
-    u-i18n
-      margin-bottom -4px
-      opacity 0.75
-      transform scale(0.76)
-      transform-origin right 50%
-
-    &>b
-      display flex
-      margin-bottom -3px
-
-      &>a
-        height 42px
-        margin-left 25px
-        margin-right 0
-        width 25px
-
-        &.ft
-          background url(':/svg/full.svg') 50% 50% / 20px no-repeat
-
-          &.f
-            background-image url(':/svg/unfull.svg')
-
-        &.gg
-          background url(':/svg/google_groups.svg') 50% 50% / 50px no-repeat
 </style>
 
