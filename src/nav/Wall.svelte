573997d5be9d8e3675956e3016fd9fcb2810dbc5
xxai.art <xxai.art@gmail.com>
2023-07-13 16:08:40 +0800

    ♨

--- src/nav/Wall.svelte
+++ src/nav/Wall.svelte
@@ -45,6 +45,7 @@ Nav
   form(@&form @submit|preventDefault)
     input(placeholder="请输入关键词")
     button(type="submit") 搜索
+  a.drop
   R(slot="R")
     Menu
     a.gg(href="//groups.google.com/g/xxai-art" slot="R" target="_blank")
@@ -52,6 +53,13 @@ Nav
 
 <style lang="stylus">
 @import './a.styl'
+
+a.drop
+  background var(--svg-nabla) no-repeat 50% 55% / 12px
+  box-sizing border-box
+  height 64px
+  border-right 1px solid #eee
+  width 64px
 
 form
   border-right 1px solid #eee
