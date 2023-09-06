7168641b04b0e5ad7cae6388a7d894c5b620504b
xxai.art <xxai.art@gmail.com>
2023-09-07 05:31:02 +0800

    ♨

--- conf/ol.js
+++ conf/ol.js
@@ -2,13 +2,9 @@ import lang from "wac.tax/_/lang.js";
 import link from '@w5/link';
 import { ver } from "./i18n/var.js";
 
-const cloudflare = '.ok0.pw'
+const cloudflare = 'ok0.pw';
+
 export const 
-  USER_TAX_CDN = link('u'+cloudflare),
-	I18N_CDN = () => link('8'+cloudflare) + ver + '/' + lang(),
-	API = link('a'+cloudflare),
-  SPI = link('f'+cloudflare),
+	I18N_CDN = () => link(cloudflare) + ver + '/' + lang(),
   RES = link('5ok.pw'),
-  META = link('m'+cloudflare);
-  //SPI = link('i-01.eu.org'),
-  //META = link('u-01.eu.org');
+  [USER_TAX_CDN,API,SPI,META] = [...'uafm'].map((i)=>link(i+'.'+cloudflare));
