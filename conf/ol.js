2dc76c1330eab4ac2544f351494f57a84db225d0
xxai.art <xxai.art@gmail.com>
2023-09-06 13:03:02 +0800

    ♨

--- conf/ol.js
+++ conf/ol.js
@@ -4,8 +4,8 @@ import { ver } from "./i18n/var.js";
 
 export const 
 	I18N_CDN = () => "//user0.cf/" + ver + "/" + lang(),
-  USER_TAX_CDN = link("ok0.pw"),
-	API = link("api.xxai.art"),
+  USER_TAX_CDN = link('ok0.pw'),
+	API = link('rmw0.tk'),
   SPI = link('i-01.eu.org'),
   RES = link('5ok.pw'),
   META = link('u-01.eu.org');
