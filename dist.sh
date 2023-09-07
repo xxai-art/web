c7ebbb66f298d85a8730e034dc12f526e132df3e
xxai.art <xxai.art@gmail.com>
2023-09-07 14:32:25 +0800

    ♨

--- dist.sh
+++ dist.sh
@@ -3,13 +3,15 @@
 DIR=$(dirname $(realpath "$0"))
 cd $DIR
 set -ex
-
+git pull
 rm -rf dist
 cd sh
 ./build.sh
 rm -rf "../dist/.18"
 ./cdn.sh
-
+git add -u
+git commit -m'dist'
+git push
 # ./sh/sw.coffee
 # cp sh/dist/* dist
 #
