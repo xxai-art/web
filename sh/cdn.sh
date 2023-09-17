8010b2421c112b04e737a3f5ef63591278cd5e00
xxai.art <xxai.art@gmail.com>
2023-09-17 18:51:07 +0800

    ♨

--- sh/cdn.sh
+++ sh/cdn.sh
@@ -4,19 +4,12 @@ DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
 set -ex
 
-direnv allow
-
-de() {
-  direnv exec . ./$1
-}
-
-de i18n.upload.coffee
+source env.sh
+./i18n.upload.coffee
 
 git add -u && git commit -m'i18n' || true
 
-cd ..
 bun x xxai-dist
-cd sh
 
-de dist.index.coffee
-de dist.public.coffee
+./dist.index.coffee
+./dist.public.coffee
