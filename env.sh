64971a8ef4e70494d9969071098729a33b8cee62
xxai.art <xxai.art@gmail.com>
2023-09-17 19:07:45 +0800

    ♨

--- env.sh
+++ env.sh
@@ -1,17 +1,3 @@
 #!/usr/bin/env bash
 
-CDN=//ok0.pw
 VITE_PORT=3333
-
-export OSSPUT_BUCKET=xxai-blog-cdn
-export BACKBLAZE_url=https://f004.backblazeb2.com/file/$OSSPUT_BUCKET
-export CDN="//ok0.pw/"
-
-export SITE_OSSPUT_BUCKET=xxai-blog-web
-export SITE_BACKBLAZE_url=https://f004.backblazeb2.com/file/$SITE_OSSPUT_BUCKET
-export SITE=xxai.art
-export BACKBLAZE_accessKeyId=
-export BACKBLAZE_secretAccessKey=
-export BACKBLAZE_endpoint=https://s3.us-west-004.backblazeb2.com
-export CLOUDFLARE_KEY=
-export CLOUDFLARE_EMAIL=
