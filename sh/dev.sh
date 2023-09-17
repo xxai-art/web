95d14ea41781269ee2c85ae421929036b75a388c
xxai.art <xxai.art@gmail.com>
2023-09-17 16:10:26 +0800

    ♨

--- sh/dev.sh
+++ sh/dev.sh
@@ -7,5 +7,4 @@ set -ex
 kill -9 $(lsof -i:$VITE_PORT -t) 2>/dev/null | true
 
 bunx concurrently --kill-others \
-  "bunx coffee -wc vite" \
-  -r "bunx vite"
+  -r "bunx vite" # "bunx coffee -wc vite" \
--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,5 @@
-vite/vite.config.js
+import uridir from '@w5/uridir'
+import viteConf from '@w5/vite-conf'
+
+export default await viteConf(uridir(import.meta))
+
